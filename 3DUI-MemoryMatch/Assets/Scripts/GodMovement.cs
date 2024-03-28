8c4c552467c1e7b6b8df91919d56887f79c62aef
Ethan Legum <71243839+elegs99@users.noreply.github.com>
2024-03-28 17:27:14 -0400

    unity auto update

--- 3DUI-MemoryMatch/Assets/Scripts/GodMovement.cs
+++ 3DUI-MemoryMatch/Assets/Scripts/GodMovement.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GodMovement : MonoBehaviour
 {
     public GameObject rightHand, leftHand;
+    public InputActionReference rightHandGrab, leftHandGrab;
     GameObject parentEnviroment;
-    bool rightHandClosed, leftHandClosed;
+    bool rightHandClosed, leftHandClosed = false;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +19,15 @@ public class GodMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (rightHandGrab.action.ReadValue<float>() > .5f) {
+            rightHandClosed = true;
+        } else {
+            rightHandClosed = false;
+        }
+        if (rightHandGrab.action.ReadValue<float>() > .5f) {
+            rightHandClosed = true;
+        } else {
+            rightHandClosed = false;
+        }
     }
 }
