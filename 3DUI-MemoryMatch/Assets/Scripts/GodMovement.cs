969f569c30af86584a341ea91e07da29c965340b
Ethan Legum <71243839+elegs99@users.noreply.github.com>
2024-03-29 19:06:24 -0400

    Translation/Rotation/Scale working
    wrote movement code for navigating puzzle
    Demo scene for movement complete

--- 3DUI-MemoryMatch/Assets/Scripts/GodMovement.cs
+++ 3DUI-MemoryMatch/Assets/Scripts/GodMovement.cs
@@ -7,27 +7,87 @@ public class GodMovement : MonoBehaviour
 {
     public GameObject rightHand, leftHand;
     public InputActionReference rightHandGrab, leftHandGrab;
-    GameObject parentEnviroment;
-    bool rightHandClosed, leftHandClosed = false;
-
+    public float scalingFactor = .1f;
+    GameObject parentEnvironment;
+    bool rightHandClosed, leftHandClosed;
+    Vector3 currentRightHandPosition, currentLeftHandPosition, lastRightHandPosition, lastLeftHandPosition;
+    float currentHandsDistance, lastHandsDistance;
+    Vector3 handMovement;
+    bool actionStarted = false;
     // Start is called before the first frame update
     void Start()
     {
-        parentEnviroment = GameObject.Find("TestEnviroment");
+        parentEnvironment = GameObject.Find("TestEnviroment");
+        lastRightHandPosition = rightHand.transform.position;
+        lastLeftHandPosition = leftHand.transform.position;
+        lastHandsDistance = Vector3.Distance(lastRightHandPosition, lastLeftHandPosition);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (rightHandGrab.action.ReadValue<float>() > .5f) {
-            rightHandClosed = true;
-        } else {
-            rightHandClosed = false;
+        currentRightHandPosition = rightHand.transform.position;
+        currentLeftHandPosition = leftHand.transform.position;
+        currentHandsDistance = Vector3.Distance(currentRightHandPosition, currentLeftHandPosition);
+        CheckIfHandsAreClosed();
+        if (rightHandClosed && leftHandClosed)
+        {
+            RotateAndScaleEnvironment();
         }
-        if (rightHandGrab.action.ReadValue<float>() > .5f) {
-            rightHandClosed = true;
+        else if (rightHandClosed || leftHandClosed)
+        {
+            TranslateEnvironment();
         } else {
-            rightHandClosed = false;
+            handMovement = Vector3.zero;
+            actionStarted = false;
         }
+        lastRightHandPosition = currentRightHandPosition;
+        lastLeftHandPosition = currentLeftHandPosition;
+        lastHandsDistance = currentHandsDistance;
+    }
+
+    void CheckIfHandsAreClosed()
+    {
+        rightHandClosed = rightHandGrab.action.ReadValue<float>() > 0.5f;
+        leftHandClosed = leftHandGrab.action.ReadValue<float>() > 0.5f;
+    }
+
+    void TranslateEnvironment()
+    {
+        if (rightHandClosed)
+        {
+            handMovement = currentRightHandPosition - lastRightHandPosition;
+        }
+        else if (leftHandClosed)
+        {
+            handMovement = currentLeftHandPosition - lastLeftHandPosition;
+        }
+        parentEnvironment.transform.position += handMovement;
+    }
+
+    void RotateAndScaleEnvironment()
+    {
+        float currentHandsDistance = Vector3.Distance(currentRightHandPosition, currentLeftHandPosition);
+
+        float handDistanceChange = currentHandsDistance - lastHandsDistance;
+        
+        // Calculate new scale
+        Vector3 newScale = parentEnvironment.transform.localScale + (Vector3.one * scalingFactor * handDistanceChange);
+        newScale = Vector3.Max(newScale, new Vector3(0.1f, 0.1f, 0.1f)); // Setting minimum scale limit
+        newScale = Vector3.Min(newScale, new Vector3(5f, 5f, 5f)); // Setting maximum scale limit
+
+        // Apply the new scale
+        parentEnvironment.transform.localScale = newScale;
+
+        Vector3 previousDirection = lastRightHandPosition - lastLeftHandPosition;
+        Vector3 currentDirection = currentRightHandPosition - currentLeftHandPosition;
+        Vector3 rotationAxis = Vector3.Cross(previousDirection, currentDirection).normalized;
+
+        // Determine the magnitude of the rotation
+        float angle = Vector3.Angle(previousDirection, currentDirection);
+
+        // Apply the rotation around the axis
+        parentEnvironment.transform.Rotate(rotationAxis, angle, Space.World);
+    
     }
 }
